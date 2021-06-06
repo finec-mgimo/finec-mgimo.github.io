20498ee29d6d8114e26805424d58578b2b878e2a
necelentano <necelentano@gmail.com>
2021-06-06 17:20:05 +0300

    Fixed error in browser console

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,31 @@
-document.getElementById('mode').addEventListener('click', () => {
+// document.getElementById('mode').addEventListener('click', () => {
 
-  document.body.classList.toggle('dark');
-  localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+//   document.body.classList.toggle('dark');
+//   localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
 
-});
+// });
 
-if (localStorage.getItem('theme') === 'dark') {
+// if (localStorage.getItem('theme') === 'dark') {
 
-  document.body.classList.add('dark');
+//   document.body.classList.add('dark');
 
-}
+// }
 
 /* eslint-disable */
-var clipboard = new ClipboardJS('.btn-clipboard');
+var clipboard = new ClipboardJS(".btn-clipboard");
 
-clipboard.on('success', function(e) {
-    /*
+clipboard.on("success", function (e) {
+  /*
     console.info('Action:', e.action);
     console.info('Text:', e.text);
     console.info('Trigger:', e.trigger);
     */
 
-    e.clearSelection();
+  e.clearSelection();
 });
 
-clipboard.on('error', function(e) {
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
+clipboard.on("error", function (e) {
+  console.error("Action:", e.action);
+  console.error("Trigger:", e.trigger);
 });
 /* eslint-enable */
