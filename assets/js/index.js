eed2e9667593b5ba971ec881ade00b161e2b8868
Mark <super16mail@gmail.com>
2022-06-09 23:54:50 +0300

    Partially resolves #119

--- assets/js/index.js
+++ assets/js/index.js
@@ -144,3 +144,87 @@ Source:
   }
 
 }());
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  const carousels = document.querySelectorAll('.carousel');
+  carousels.forEach(function( carousel ) {
+
+    const ele = carousel.querySelector('ul');
+    const scrolllength = carousel.querySelector('ul li:nth-child(2)').offsetLeft - carousel.querySelector('ul li:nth-child(1)').offsetLeft;
+    const amountvisible = Math.round(ele.offsetWidth/scrolllength);
+    const bullets = carousel.querySelectorAll('ol li');
+    const nextarrow = carousel.querySelector('.next');
+    const prevarrow = carousel.querySelector('.prev');
+    bullets[0].classList.add('selected');
+    var removeels = carousel.querySelectorAll('ol li:nth-last-child(-n + '+(amountvisible-1)+')');
+    removeels.forEach(function(removeel) {
+      removeel.remove();
+    });
+
+    const setSelected = function() {
+        bullets.forEach(function(bullet) {
+           bullet.classList.remove('selected');
+        });
+        let nthchild = (Math.round((ele.scrollLeft/scrolllength)+1));
+        carousel.querySelector('ol li:nth-child('+nthchild+')').classList.add('selected');
+    }
+
+    const scrollTo = function(event) {
+      event.preventDefault();
+      ele.scrollLeft = ele.querySelector(this.getAttribute('href')).offsetLeft;
+    }
+
+    const nextSlide = function() {
+      if(!carousel.querySelector('ol li:last-child').classList.contains('selected')) {
+        carousel.querySelector('ol li.selected').nextElementSibling.querySelector('a').click();
+      } else {
+        carousel.querySelector('ol li:first-child a').click();
+      }
+    }
+
+    const prevSlide = function() {
+      if(!carousel.querySelector('ol li:first-child').classList.contains('selected')) {
+        carousel.querySelector('ol li.selected').previousElementSibling.querySelector('a').click();
+      } else {
+        carousel.querySelector('ol li:last-child a').click();
+      }
+    }
+
+    ele.addEventListener("scroll", debounce (setSelected));
+    nextarrow.addEventListener("click", nextSlide);
+    prevarrow.addEventListener("click", prevSlide);
+    bullets.forEach(function(bullet) {
+        bullet.querySelector('a').addEventListener('click', scrollTo);
+    });
+
+    if(carousel.getAttribute('duration')) {
+      setInterval(function(){
+      if (ele != document.querySelector(".carousel:hover ul")) {
+          nextarrow.click();
+      }
+      }, carousel.getAttribute('duration'));
+    }
+
+  });
+
+});
+
+/**
+ * Debounce functions for better performance
+ * (c) 2021 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param  {Function} fn The function to debounce
+ */
+function debounce (fn) {
+	let timeout;
+	return function () {
+		let context = this;
+		let args = arguments;
+		if (timeout) {
+			window.cancelAnimationFrame(timeout);
+		}
+		timeout = window.requestAnimationFrame(function () {
+			fn.apply(context, args);
+		});
+	};
+}
