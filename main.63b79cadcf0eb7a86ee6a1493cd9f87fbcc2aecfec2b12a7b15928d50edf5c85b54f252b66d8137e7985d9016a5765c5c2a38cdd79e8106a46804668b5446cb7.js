d22af8ccf2791247c4ed90628e956e5a6ec10858
super16 <super16@users.noreply.github.com>
2022-07-18 20:42:20 +0000

    deploy: 54ef8fc61aadbb306a4745e95bce47a551fa5c27

--- main.63b79cadcf0eb7a86ee6a1493cd9f87fbcc2aecfec2b12a7b15928d50edf5c85b54f252b66d8137e7985d9016a5765c5c2a38cdd79e8106a46804668b5446cb7.js
+++ main.63b79cadcf0eb7a86ee6a1493cd9f87fbcc2aecfec2b12a7b15928d50edf5c85b54f252b66d8137e7985d9016a5765c5c2a38cdd79e8106a46804668b5446cb7.js
@@ -127,4 +127,3 @@ prefetcher.href=url
 document.head.appendChild(prefetcher)
 prefetches.add(url)}
 ;
-var clipboard=new ClipboardJS(".btn-clipboard");clipboard.on("success",function(e){e.clearSelection();});clipboard.on("error",function(e){console.error("Action:",e.action);console.error("Trigger:",e.trigger);});
